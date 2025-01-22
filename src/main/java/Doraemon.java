2baba26f3e00d2ffbf95f0c15e012b4e7e41e2e1
TiangSoonYong <tiangsoonyong@gmail.com>
2025-01-23 00:43:44 +0800

    Rename, Greet, Exit
    Renamed Duke to Doraemon
    Implemented initial skeletal version
    Removed traces of Duke from source code

---
+++ src/main/java/Doraemon.java
@@ -0,0 +1,11 @@
+public class Doraemon {
+    public static void main(String[] args) {
+        System.out.println("__________________________________________________");
+        System.out.println("Hello! I'm Doraemon!");
+        System.out.println("What can I do for you?");
+        System.out.println("__________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("__________________________________________________");
+    }
+}
+
