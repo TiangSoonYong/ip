b8718e9608df999adccf3716cedb9f3af81a0f1f
TiangSoonYong <tiangsoonyong@gmail.com>
2025-02-02 22:48:48 +0800

    Echo
    Improved skeletal version by adding Echo capability
    Added Scanner library
    Created printOut method for printing output

--- src/main/java/Doraemon.java
+++ src/main/java/Doraemon.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class Doraemon {
     public static void main(String[] args) {
-        System.out.println("__________________________________________________");
-        System.out.println("Hello! I'm Doraemon!");
-        System.out.println("What can I do for you?");
-        System.out.println("__________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("__________________________________________________");
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        line = "\tHello! I'm Doraemon!\n\tWhat can I do for you?";
+        printOut(line);
+        line = in.nextLine();
+
+        while(!line.equals("bye")){
+            printOut("\t"+line);
+            line = in.nextLine();
+        }
+        line = "\tBye. Hope to see you again soon!";
+        printOut(line);
+    }
+    private static void printOut(String line){
+        System.out.println("\t__________________________________________________");
+        System.out.println(line);
+        System.out.println("\t__________________________________________________");
     }
 }
 
