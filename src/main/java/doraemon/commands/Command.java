4a45abe91e7a16ce6d383895e673ac4ebef84ef8
TiangSoonYong <tiangsoonyong@gmail.com>
2025-02-26 02:29:04 +0800

    Minor refactoring
    For code clarity

--- src/main/java/doraemon/commands/Command.java
+++ src/main/java/doraemon/commands/Command.java
@@ -3,9 +3,13 @@ package doraemon.commands;
 import doraemon.Formatter;
 import doraemon.Storage;
 import doraemon.exceptions.AddTaskException;
+import doraemon.exceptions.DoraemonException;
+import doraemon.exceptions.InvalidKeywordException;
 import doraemon.exceptions.InvalidTaskNumberException;
 import doraemon.TaskManager;
 
+import java.util.ArrayList;
+
 public abstract class Command {
-    public abstract void execute(TaskManager taskManager, Formatter formatter, Storage storage) throws AddTaskException, InvalidTaskNumberException;
+    public abstract void execute(TaskManager taskManager, Formatter formatter, Storage storage) throws DoraemonException;
 }
--- src/main/java/doraemon/commands/MarkCommand.java
+++ src/main/java/doraemon/commands/MarkCommand.java
@@ -6,18 +6,18 @@ import doraemon.exceptions.InvalidTaskNumberException;
 import doraemon.TaskManager;
 
 public class MarkCommand extends Command {
-    private final String commandArgs;
+    private final String taskNumber;
     private final boolean isDone;
 
-    public MarkCommand(String commandArgs, boolean isDone) {
-        this.commandArgs = commandArgs;
+    public MarkCommand(String taskNumber, boolean isDone) {
+        this.taskNumber = taskNumber;
         this.isDone = isDone;
     }
 
     @Override
     public void execute(TaskManager taskManager, Formatter formatter, Storage storage) throws InvalidTaskNumberException {
         try {
-            String feedback = taskManager.setIsDone(Integer.parseInt(this.commandArgs) - 1, this.isDone);
+            String feedback = taskManager.setIsDone(Integer.parseInt(this.taskNumber) - 1, this.isDone);
             formatter.echo(feedback);
         } catch (NumberFormatException e) {
             throw new InvalidTaskNumberException();
