f86336f6a942f904d909bf0b6c962ea7521db597
TiangSoonYong <tiangsoonyong@gmail.com>
2025-02-26 02:36:34 +0800

    Added usage info for find command

--- src/main/java/doraemon/commands/Command.java
+++ src/main/java/doraemon/commands/Command.java
@@ -2,10 +2,9 @@ package doraemon.commands;
 
 import doraemon.Formatter;
 import doraemon.Storage;
-import doraemon.exceptions.AddTaskException;
-import doraemon.exceptions.InvalidTaskNumberException;
+import doraemon.exceptions.DoraemonException;
 import doraemon.TaskManager;
 
 public abstract class Command {
-    public abstract void execute(TaskManager taskManager, Formatter formatter, Storage storage) throws AddTaskException, InvalidTaskNumberException;
+    public abstract void execute(TaskManager taskManager, Formatter formatter, Storage storage) throws DoraemonException;
 }
